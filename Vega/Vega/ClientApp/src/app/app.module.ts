e2344d1cca8325c9530213231707cbd9b5c0d410
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-08-20 19:30:57 +0300

    Add vehicle form component and routing for it

--- Vega/Vega/ClientApp/src/app/app.module.ts
+++ Vega/Vega/ClientApp/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    VehicleFormComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -24,6 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'vehicle/new', component: VehicleFormComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
---
+++ Vega/Vega/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-vehicle-form',
+    templateUrl: './vehicle-form.component.html',
+    styleUrls: ['./vehicle-form.component.css']
+})
+
+export class VehicleFormComponent implements OnInit{
+    /**
+     *
+     */
+    constructor() {
+    }
+
+    ngOnInit(){
+
+    }
+}
