ab472ddd1dbbe3aee4e14addf734e4a2c0d1b1cb
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-08-20 19:02:01 +0300

    Add Feature Controller with table and migration.

---
+++ Vega/Vega/Controllers/FeatureController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Models;
+using Vega.Persistence;
+using Vega.Resources;
+
+namespace Vega.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FeatureController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/api/features")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await _context.Features.ToListAsync();
+            return _mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
---
+++ Vega/Vega/Migrations/20200820152954_SeedFeatures.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature_1')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature_2')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature_3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name IN ('Feature_1', 'Feature_2', 'Feature_3')");
+        }
+    }
+}
